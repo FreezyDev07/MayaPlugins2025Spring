c95bd37100a31d5ea8da8f391c37dc6fefcc7f40
FreezyDev07 <freezydev07@gmail.com>
2025-04-22 16:07:48 -0500

    Update MayaToUE.py

--- src/MayaToUE.py
+++ src/MayaToUE.py
@@ -1,13 +1,22 @@
 from MayaUtils import *
-from PySide2.QtWidgets import QVBoxLayout
+from PySide2.QtGui import QIntValidator, QRegExpValidator
+from PySide2.QtWidgets import QCheckBox, QHBoxLayout, QLabel, QLineEdit, QListWidget, QMessageBox, QPushButton, QVBoxLayout
 import maya.cmds as mc
 
+def TryAction(actionFunc):
+    def wrapper(*args, **kwargs):
+        try:
+            actionFunc(*args, **kwargs)
+        except Exception as e:
+            QMessageBox().critical(None, "error!", f"{e}")
+
+    return wrapper
 
 class AnimClip:
     def __init__(self):
         self.subfix = ""
-        self.framMin = mc.playbackOptions(q=True, min=True)
-        self.framMax = mc.playbackOptions(q=True, max=True)
+        self.frameMin = mc.playbackOptions(q=True, min=True)
+        self.frameMax = mc.playbackOptions(q=True, max=True)
         self.shouldExport = True
 
 class MayaToUE:
@@ -18,15 +27,181 @@ class MayaToUE:
         self.fileName = ""
         self.saveDir = ""
 
+    def AddNewAnimClip(self):
+        self.animations.append(AnimClip())
+        return self.animations[-1]
 
-        class MayaToUEWidget(MayaWindow):
-            def GetWidgetUniqueName(self):
-                return "MayaToUEWidgetJS4172025408"
+    def AddSelectedMeshes(self):
+        selection = mc.ls(sl=True)
+
+        meshes = []
+        if not selection:
+            raise Exception("No mesh selected, please select all the meshes of your rig")
+        
+        meshes = []
+        for sel in selection:
+            if IsMesh(sel):
+                meshes.append(sel)
+
+        if len(meshes) == 0:
+            raise Exception("No mesh selected, please select all the meshes of your rig")
+
+        self.models = meshes
+
+
+    def AddRootJoint(self):
+        if not self.rootJnt:
+            raise Exception("No root joint assigned, please set the root joint of your rig first")
+        
+        if mc.objExists(self.rootJnt):
+            currentRootPos = mc.xform(self.rootJnt, q=True, ws=True, t=True)
+            if currentRootPos[0] == 0 and currentRootPos[1] == 0 and currentRootPos[2] == 0:
+                raise Exception("Current root joint is at origin already, no need to make a new one!")
             
-            def __init__(self):
-                super().__init__()
-                self.setWindowTitle("Maya to UE")
-                self.masterLayout = QVBoxLayout()
-                self.setLayout(self.masterLayout)
+        mc.select(cl=True)
+        rootJntname = self.rootJnt + "_root"
+        mc.joint(n=rootJntname)
+        mc.parent(self.rootJnt, rootJntname)
+        self.rootJnt = rootJntname
 
-        MayaToUEWidget().show()
+
+    def SetSelectedJointAsRoot(self):
+        selection = mc.ls(sl=True, type="joint")
+        if not selection:
+            raise Exception("Wrong selection, please select the root joint of your rig!")
+        
+        self.rootJnt = selection[0]
+
+
+class AnimClipWidget(QWidget):
+    def __init__(self, animClip: AnimClip):
+        super().__init__()
+        self.animClip = animClip
+        self.masterLayout = QHBoxLayout()
+        self.setLayout(self.masterLayout)
+
+        shouldExportCheckbox = QCheckBox()
+        shouldExportCheckbox.setChecked(self.animClip.shouldExport)
+        self.masterLayout.addWidget(shouldExportCheckbox)
+        shouldExportCheckbox.toggled.connect(self.ShouldExportCheckboxToggled)
+
+        subfixLabel = QLabel("Subfix: ")
+        self.masterLayout.addWidget(subfixLabel)
+
+        subfixLineEdit = QLineEdit()
+        subfixLineEdit.setValidator(QRegExpValidator("[a-zA-z0-9_]+"))
+        subfixLineEdit.setText(self.animClip.subfix)
+        subfixLineEdit.textChanged.connect(self.SubfixTextChanged)
+        self.masterLayout.addWidget(subfixLineEdit)
+
+        minFrameLabel = QLabel("min: ")
+        self.masterLayout.addWidget(minFrameLabel)
+        minFrameLineEdit = QLineEdit()
+        minFrameLineEdit.setValidator(QIntValidator())
+        minFrameLineEdit.setText(str(int(self.animClip.frameMin)))
+        minFrameLineEdit.textChanged.connect(self.MinFrameChanged)
+        self.masterLayout.addWidget(minFrameLineEdit)
+
+        maxFrameLabel = QLabel("max: ")
+        self.masterLayout.addWidget(maxFrameLabel)
+        maxFrameLineEdit = QLineEdit()
+        maxFrameLineEdit.setValidator(QIntValidator())
+        maxFrameLineEdit.setText(str(int(self.animClip.frameMax)))
+        maxFrameLineEdit.textChanged.connect(self.MaxFrameChanged)
+        self.masterLayout.addWidget(maxFrameLineEdit)
+
+        setRangeBtn = QPushButton("[-]")
+        setRangeBtn.clicked.connect(self.SetRangeBtnClicked)
+        self.masterLayout.addWidget(setRangeBtn)
+
+        deleteBtn = QPushButton("X")
+        deleteBtn.clicked.connect(self.DeleteBtnClicked)
+        self.masterLayout.addWidget(deleteBtn)
+
+    def DeleteBtnClicked(self):
+        self.deleteLater()
+
+    def SetRangeBtnClicked(self):
+        mc.playbackOptions(e=True, min = self.animClip.frameMin, max = self.animClip.frameMax)
+        mc.playbackOptions(e=True, ast = self.animClip.frameMin, aet = self.animClip.frameMax)
+
+
+
+    def MinFrameChanged(self, newVal):
+        self.animClip.frameMin = int(newVal)
+
+    def MaxFrameChanged(self, newVal):
+        self.animClip.frameMax = int(newVal)
+
+    def SubfixTextChanged(self, newText):
+        self.animClip.subfix = newText
+
+    def ShouldExportCheckboxToggled(self):
+        self.animClip.shouldExport = not self.animClip.shouldExport
+
+
+class MayaToUEWidget(MayaWindow):
+    def GetWidgetUniqueName(self):
+        return "MayaToUEWidgetJS4172025408"
+            
+    def __init__(self):
+        super().__init__()
+        self.mayaToUE = MayaToUE()
+
+        self.setWindowTitle("Maya to UE")
+        self.masterLayout = QVBoxLayout()
+        self.setLayout(self.masterLayout)
+
+        self.rootJntText = QLineEdit()
+        self.rootJntText.setEnabled(False)
+        self.masterLayout.addWidget(self.rootJntText)
+
+        setSelectedAsRootJntBtn = QPushButton("set Root Joint")
+        setSelectedAsRootJntBtn.clicked.connect(self.SetSelectedAsRootJntBtnClicked)
+        self.masterLayout.addWidget(setSelectedAsRootJntBtn)
+
+        addRootJntBtn = QPushButton("Add Root Joint")
+        addRootJntBtn.clicked.connect(self.AddRootJntBtnClicked)
+        self.masterLayout.addWidget(addRootJntBtn)
+
+        self.meshList = QListWidget()
+        self.masterLayout.addWidget(self.meshList)
+        self.meshList.setMaximumHeight(100)
+
+        addMeshesBtn = QPushButton("Add meshes")
+        addMeshesBtn.clicked.connect(self.AddMeshesBtnClicked)
+        self.masterLayout.addWidget(addMeshesBtn)
+
+        addAnimEntryBtn = QPushButton("Add animation clip")
+        addAnimEntryBtn.clicked.connect(self.addAnimEntryBtnClicked)
+        self.masterLayout.addWIdget(addAnimEntryBtn)
+
+        self.animClipEntryLayout = QVBoxLayout()
+        self.masterLayout.addLayout(self.animClipEntryLayout)
+
+    @TryAction
+    def AddAnimEntryBtnClicked(self):
+        newAnimClip = self.mayaToUE.AddNewAnimClip()
+        newAnimClipWidget = AnimClipWidget(newAnimClip)
+        self.animClipEntryLayout.addWidget(newAnimClipWidget)
+
+
+    @TryAction
+    def AddMeshesBtnClicked(self):
+        self.mayaToUE.AddSelectedMeshes()
+        self.meshList.clear()
+        self.meshList.addItems(self.mayaToUE.models)
+    
+
+    @TryAction
+    def AddRootJntBtnClicked(self):
+        self.mayaToUE.AddRootJoint()
+        self.rootJntText.setText(self.mayaToUE.rootJnt)
+
+    @TryAction
+    def SetSelectedAsRootJntBtnClicked(self):
+        self.mayaToUE.SetSelectedJointAsRoot()
+        self.rootJntText.setText(self.mayaToUE.rootJnt)
+
+
+MayaToUEWidget().show()
